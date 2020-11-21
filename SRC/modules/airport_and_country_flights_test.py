b38282f3a971ff36ea00195ec24e3e32de9ea46b
Mario Loera <marioll@kth.se>
2020-11-21 21:17:51 +0100

    add 3 Unit test (#5)
    * add airports_test.py
    * add country_flights_test
    * add test data files  & add airport_and_country_flights_test

---
+++ SRC/modules/airport_and_country_flights_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import os
+import csv
+import unittest
+import airports
+import country_flights
+
+
+class TestFlightPerCountry(unittest.TestCase):
+
+    src_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(src_dir, '..', '..', 'input_data')
+
+    airports_file = os.path.join(data_dir, 'airports_test.dat')
+    flights_file = os.path.join(data_dir, 'routes_test.dat')
+
+    def test_country_flight_files(self):
+
+        # process_airport_file
+        airport1 = airports.Airport()
+        with open(self.airports_file, 'r', encoding='UTF-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                airport1.process_airport_row(row)
+
+        # process_flight_file
+        flight_per_country = country_flights.FlightPerCountry(
+            airport1.airports_by_iata)
+        with open(self.flights_file, 'r', encoding='UTF-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                flight_per_country.process_flight(row)
+
+        expected = {
+            "Afghanistan": [14, 29],
+            "Congo (Brazzaville)": [10, 34],
+            "Denmark": [23, 286],
+            "Guinea-Bissau": [0, 7],
+            "Sweden": [167, 307],
+            flight_per_country.unknown_country: [93, 636],
+        }
+
+        result = {}
+        for country in expected:
+            result[country] = flight_per_country.countries[country]
+
+        self.assertEqual(result, expected)
+
+
+unittest.main()
---
+++ SRC/modules/country_flights_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import unittest
+import country_flights
+
+
+class TestFlightPerCountry(unittest.TestCase):
+
+    IataCountry = {
+        "ARN": "Sweden",
+        "GOT": "Sweden",
+        "LAX": "United States",
+        "ELP": "United States",
+    }
+
+    AirpotIdCountry = {
+        737: "Sweden",
+        687: "Sweden",
+        3484: "United States",
+        3559: "United States",
+    }
+
+    FlightDataA = [
+        (00, 000, "ARN", 737, "GOT", 687, 0, 0, 135),
+        (00, 000, "ARN", 737, "ELP", 3559, 0, 0, 135),
+        (00, 410, "LAX", 2965, "GOT", 687, 0, 0, 135),
+        (00, 410, "LAX", 2966, "ELP", 3559, 0, 0, 135),
+        (00, 410, "LAX", 2966, "XXX", -1, 0, 0, 135),
+        (00, 410, "XXX", -1, "ELP", 3559, 0, 0, 135),
+        (00, 410, "XXX", -1, "YYY", -2, 0, 0, 135),
+    ]
+
+    def test_get_airport_country_by_iata(self):
+        flightPerCountry = country_flights.FlightPerCountry(self.IataCountry)
+        expected = self.IataCountry
+        result = {}
+        for iata in self.IataCountry:
+            result[iata] = flightPerCountry.get_airport_country(
+                airport_code=iata)
+        self.assertEqual(result, expected)
+
+    def test_get_unknown_country(self):
+        flightPerCountry = country_flights.FlightPerCountry({})
+        expected = flightPerCountry.unknown_country
+        result = flightPerCountry.get_airport_country('KIO')
+        self.assertEqual(result, expected)
+
+    def test_get_country_flights(self):
+        flightPerCountry = country_flights.FlightPerCountry(self.IataCountry)
+
+        expected = {
+            "Sweden": [1, 1],
+            "United States": [1, 1],
+            flightPerCountry.unknown_country: [1, 2],
+        }
+
+        for row in self.FlightDataA:
+            flightPerCountry.process_flight(row)
+
+        result = {}
+        for country in expected:
+            result[country] = flightPerCountry.countries[country]
+
+        self.assertEqual(result, expected)
+
+
+unittest.main()
