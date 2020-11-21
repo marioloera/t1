74ecc55cee04d01ea56ca171221974ce6e90b78a
Mario Loera <marioll@kth.se>
2020-11-21 22:00:26 +0100

    code format

--- SRC/flights.py
+++ SRC/flights.py
@@ -5,11 +5,13 @@ from modules import airports
 from modules import country_flights
 import argparse
 
+
 def main():
 
     # output file
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output_file", help="output file where results are stored")
+    parser.add_argument("--output_file",
+                        help="output file where results are stored")
     args = parser.parse_args()
 
     src_dir = os.path.dirname(os.path.realpath(__file__))
@@ -22,12 +24,14 @@ def main():
 
     # proces flights
     flights_file = os.path.join(data_dir, 'routes.dat')
-    flight_per_country = country_flights.FlightPerCountry(airport1.airports_by_iata)
+    flight_per_country = country_flights.FlightPerCountry(
+        airport1.airports_by_iata)
     process_flight_file(flights_file, flight_per_country)
-    
+
     # results
     flight_per_country.print_results()
-    print(args.output_file)
+    flight_per_country.save_results(args.output_file)
+
 
 def process_airport_file(file_path, _airport):
     try:
