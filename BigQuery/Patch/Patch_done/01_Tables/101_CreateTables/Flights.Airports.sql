74010a798d53b968af859d10ab9329cf25cd5dda
Mario Loera <marioll@kth.se>
2020-11-21 23:09:21 +0100

    first deploy 01_Tables

---
+++ BigQuery/Patch/Patch_done/01_Tables/101_CreateTables/Flights.Airports.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS Flights.Airports (
+
+    AirportID STRING,
+    Name STRING,
+    City STRING,
+    Country STRING,
+    IATA STRING,
+    ICAO STRING,
+    Latitude STRING,
+    Longitude STRING,
+    Altitude STRING,
+    Timezone STRING,
+    DST STRING,
+    TzDatabaseTimeZone STRING,
+    Type STRING,
+    Source STRING,
+    );
