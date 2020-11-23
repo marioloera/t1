21cc49b25da5b88dfe589139708b48cd4967e608
Mario Loera <marioll@kth.se>
2020-11-23 14:51:03 +0100

    fix comment typos

--- SRC/flights.py
+++ SRC/flights.py
@@ -17,12 +17,12 @@ def main():
     src_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = os.path.join(src_dir, '..', 'input_data')
 
-    # proces airports
+    # process airports
     airports_file = os.path.join(data_dir, 'airports.dat')
     airport1 = airports.Airport()
     process_airport_file(airports_file, airport1)
 
-    # proces flights
+    # process flights
     flights_file = os.path.join(data_dir, 'routes.dat')
     flight_per_country = country_flights.FlightPerCountry(
         airport1.airports_by_iata)
@@ -48,8 +48,8 @@ def process_airport_file(file_path, _airport):
 def process_flight_file(file_path, _flight_per_country):
     '''
     process the csv data in the file
-    and call the funtion process_flight to get the 
-    domistic and international flights for each country
+    and call the function process_flight to get the
+    domestic and international flights for each country
     '''
     try:
         with open(file_path, 'r', encoding='UTF-8') as f:
