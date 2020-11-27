e1579389672eb3a70334480fe77fc3507a353276
Mario Loera <marioll@kth.se>
2020-11-27 14:19:29 +0100

    add module flights with clas FlightsColumns and attribute __list__

---
+++ SRC/modules/flights.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import logging
+
+
+class FlightsColumns:
+
+    airline = 0
+    airline_id = 1
+    source_airport = 2
+    source_airport_id = 3
+    destination_airport = 4
+    destination_airport_id = 5
+    codeshare = 6
+    stops = 7
+    equipment = 8
+
+    __list__ = [
+        name for name in tuple(locals())
+        if not (name.startswith('__') and name.endswith('__'))
+    ]
