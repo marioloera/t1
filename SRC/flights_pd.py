da547857e1ea2c92a8c37cc26608d470c262caed
Mario Loera <marioll@kth.se>
2020-11-27 16:07:20 +0100

    getting domestic and internation flights columns

--- SRC/flights_pd.py
+++ SRC/flights_pd.py
@@ -45,8 +45,7 @@ def main():
     flights_df_all = pd.read_csv(flights_file, names=flights_col.__list__)
 
     # remove columns not needed so the join contains less columns
-    flights_df = flights_df_all[0:10]
-    flights_df = flights_df_all.iloc[:, [
+    flights_df = flights_df_all.iloc[9:15, [
         flights_col.source_airport,
         flights_col.destination_airport,
     ]]
@@ -63,27 +62,46 @@ def main():
     #     print(i, source_airport, source_airport_id, destination_airport,
     #           destination_airport_id)
 
-    # pands merge by col names
+    # pands merge by col names source airpot ai
     src1 = flights_df.merge(airports_df,
                             left_on='source_airport',
                             right_on='iata')
 
-    print('\nsrc1\n')
-    print(src1)
-    print(src1.columns)
-    # x2 = flights_df.merge(airports_df, left_on=flights_df.columns[flights_col.source_airport], right_on=airports_df.columns[airports_col.iata])
-    # print(x2)
+    # print('\nsrc1\n')
+    # print(src1)
+    # print(src1.columns)
+    # # x2 = flights_df.merge(airports_df, left_on=flights_df.columns[flights_col.source_airport], right_on=airports_df.columns[airports_col.iata])
+    # # print(x2)
 
-    print(src1[['source_airport', 'destination_airport', 'country']])
+    # print(src1[['source_airport', 'destination_airport', 'country']])
 
     src2 = src1.merge(airports_df,
                       left_on='destination_airport',
-                      right_on='iata')
+                      right_on='iata',
+                      suffixes=('_src', '_dest'))
 
     print('\nsrc2\n')
-    print(src2)
-    print(src2.columns)
-    #print(src2[['airline', 'airline_id', 'country']])
+    #print(src2)
+    #print(src2.columns)
+    print(src2[['country_src', 'country_dest']])
+
+    # add domestic or international
+
+    #src2['test'] = src2.apply(lambda row: row.country_src + row.country_dest, axis=1)
+    # fn = lambda row: row.country_src + row.country_dest
+    # fn = lambda row: row.country_src == row.country_dest
+    # src2['test2'] = src2.apply(fn, axis=1)
+
+    fn_domestic = lambda row: 1 if row.country_src == row.country_dest else 0
+    fn_international = lambda row: 1 if row.country_src != row.country_dest else 0
+    src2['domestic'] = src2.apply(fn_domestic, axis=1)
+    src2['international'] = src2.apply(fn_international, axis=1)
+
+    # df1.iloc[1:5, 2:4]
+    print(src2.iloc[:, 2:])
+
+    # TODO group by country_src, sum domestic, sum (international)
+
     msg = f'Process completed!'
     # logging.info(msg)
     print(msg)
